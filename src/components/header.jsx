1b67bd36108563ca83ec71b46f981e1f1e353d28
Mariana0200 <marianarodrigues153@gmail.com>
2026-07-14 15:17:46 +0100

    Firebase Git React

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+      <header className="header">
+        <img
+          src="/images/logo.png"
+          alt="University of Nottingham logo"
+          className="logo"
+        />
+  
+        <h2>Confocal Dye Assistant</h2>
+      </header>
+    );
+  }
+  
+  export default Header;
